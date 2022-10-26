d71198f6200503512dedff61653c7b0a42bc3cae
chioma2510 <edehchiomajoy@gmail.com>
2022-10-26 09:58:23 -0700

    Task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - Change all lowercase letters to uppercase
+ * @c: string
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 96 && c[i] < 123)
+		{
+			c[i] -= 32;
+		}
+	}
+	return (c);
+}
+
